f0d959adff6cb349aee4cb0b5d4aa925861e99ae
georgelee144 <george@ml.cafe>
2024-09-11 22:18:05 -0400

    initial commit

---
+++ helper.py
@@ -0,0 +1,33 @@
+import toml
+
+days_of_week = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+]
+
+
+def read_toml(file):
+    return toml.load(file)
+
+def read_vs_day_toml(file="vs_day_reminders.toml"):
+    return read_toml(file)
+
+def read_server_info(file="server_info.toml"):
+    return read_toml(file)
+
+def read_token(file=".secret"):
+    with open(file=file) as f:
+        token = f.read()
+        return token
+
+def combine_vs_tasks(tasks):
+    message = ""
+    for index,task in enumerate(tasks):
+        message += f"{index+1}. {task}\n"
+
+    return message
