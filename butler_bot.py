bd27fc39dfbfe4553084e207024cc9ae31a28a25
georgelee144 <43113880+georgelee144@users.noreply.github.com>
2024-09-15 01:53:09 -0400

    added more tips and reminders to messaging, changed scheduler, and removed useless comments (#6)
    closes issue 5

--- butler_bot.py
+++ butler_bot.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 import pendulum
 import helper
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -19,7 +20,6 @@ async def send_message_to_channel(channel, message):
 
     await channel.send(message)
 
-
 @client.event
 async def on_member_join(member):
     print(f"New member joined: {member.name}")
@@ -29,44 +29,35 @@ async def on_member_join(member):
     else:
         print("Error: Could not find the general channel.")
 
-
-# @client.event
-# async def on_member_join(member):
-#     print("triggerd")
-
-#     alliance_name = server_info["alliance_name"]
-
-#     message = f"Hello {member.mention}!\nWelcome to {alliance_name}.\nVisit for the rules: {server_info['rules_url']}"
-#     await send_message_to_channel("welcome_channel_id", message)
-
-
 async def vs_day_reminder():
     vs_day_reminders = helper.read_vs_day_toml()
+    current_time = pendulum.now()
 
-    while True:
-        current_time = pendulum.now()
-        if current_time.hour == 22 and current_time.minute == 0:
-            # if True:
+    for vs_day_key, vs_day_info in vs_day_reminders.items():
+        day_of_week_match_check = (
+            helper.days_of_week[current_time.day_of_week] == vs_day_info["day"]
+        )
 
-            for vs_day_key, vs_day_info in vs_day_reminders.items():
-                day_of_week_match_check = (
-                    helper.days_of_week[current_time.day_of_week] == vs_day_info["day"]
-                )
+        if day_of_week_match_check:
+            message = (
+                f"# It is now day {vs_day_key[-1]} aka {vs_day_info['name']}.\n"
+            )
+            message += "## Perform the following tasks to earn points:\n"
+            message += f"""{helper.combine_vs_messages(vs_day_info["tasks"])}"""
+            message += "## Some tips for this day:\n"
+            message += f"""{helper.combine_vs_messages(vs_day_info["tips"])}"""
+            message += "## Some reminders for upcoming days:\n"
+            message += f"""{helper.combine_vs_messages(vs_day_info["reminders"])}"""
+            message += "### For questions and a complete guide please visit [vs-tournament-tips-info](https://discord.com/channels/1263660916994478120/1264795050941091941)"
+            break
 
-                if day_of_week_match_check:
-                    message = (
-                        f"# It is now day {vs_day_key[-1]} aka {vs_day_info['name']}.\n"
-                    )
-                    message += "Perform the following tasks to earn points:\n"
-                    message += f"""{helper.combine_vs_tasks(vs_day_info["tasks"])}"""
-                    break
-            await send_message_to_channel("annoncement_channel_id", message)
-            await asyncio.sleep(90)
-
+    await send_message_to_channel("annoncement_channel_id", message)
+    await asyncio.sleep(61)
 
 @client.event
 async def on_ready():
-    client.loop.create_task(vs_day_reminder())
-
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(vs_day_reminder, 'cron', hour=1, minute=49)
+    scheduler.start()
 
 client.run(token)
